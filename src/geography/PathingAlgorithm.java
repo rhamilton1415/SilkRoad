93ad99a2ce10d4050ecd0263112e7414ada2e8e6
rhamilton1415 <roberthamilton12@hotmail.co.uk>
2017-10-01 12:49:04 +0100

    Pathing algorithm methods created

---
+++ src/geography/PathingAlgorithm.java
@@ -0,0 +1,12 @@
+package geography;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PathingAlgorithm 
+{
+	public static ArrayList<Province> AStar(Collection<? extends Tile> tileSet, Tile start, Tile target)
+	{
+		return null;
+	}
+}
